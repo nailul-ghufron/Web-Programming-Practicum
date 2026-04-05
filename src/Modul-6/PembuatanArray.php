dfbbf3971ba73e351ae5cdb4af2bdb7fb108b887
Nailul <240605110160@student.uin-malang.ac.id>
2026-04-06 05:58:32 +0700

    Update modul 6

---
+++ src/Modul-6/PembuatanArray.php
@@ -0,0 +1,7 @@
+<?php
+$hari = ['Sen' => 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+
+foreach ($hari as $no => $nama_hari) {
+    echo "Hari ke-" . $no . " adalah " . $nama_hari . "<br>";
+}
+?>
